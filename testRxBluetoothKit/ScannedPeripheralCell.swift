71ad251b6c54ffc460b1ad2d108f215ed0399ce3
BlueYang <77963964@qq.com>
2017-06-20 00:46:04 +0800

    add gitignore

---
+++ testRxBluetoothKit/ScannedPeripheralCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScannedPeripheralCell.swift
+//  testRxBluetoothKit
+//
+//  Created by  BlueYang on 2017/6/19.
+//  Copyright © 2017年  BlueYang. All rights reserved.
+//
+
+import UIKit
+
+class ScannedPeripheralCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
